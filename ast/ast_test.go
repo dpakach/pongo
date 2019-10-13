ab26b8bb4c25a6335b22e72ad4e37410e905ffcb
Dipak Acharya <dpakach@gmail.com>
2019-10-13 14:57:48 +0545

    Initial Commit
    Add Lexer for tokenization of program elements
    Add Ast elements for storing the program structure in better structure.
    Add Parser for parsing the program and construct the relevent AST
    Add simple repl that just displays the tokens in the program

---
+++ ast/ast_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"gorilla/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	programString := "let myVar = anotherVar;"
+	program := &Program {
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != programString {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
---
+++ token/token.go
@@ -0,0 +1,57 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL="ILLEGAL"
+	EOF="EOF"
+	IDENT="IDENT"
+	INT="INT"
+	ASSIGN="="
+	COMMA=","
+	SEMICOLON=";"
+	LPAREN="("
+	RPAREN=")"
+	LBRACE="{"
+	RBRACE="}"
+	// Operators
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET= "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	EQ = "=="
+	NOT_EQ = "NOT_EQ"
+)
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
