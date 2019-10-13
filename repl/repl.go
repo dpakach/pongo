9d1fd644e3d67c6eb1cce0df19bea2d66bc883c1
Dipak Acharya <dpakach@gmail.com>
2019-10-13 15:52:38 +0545

    Update REPL to show the parsed program string representation

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"gorilla/lexer"
-	"gorilla/token"
+	"gorilla/parser"
 )
 
 const PROMPT = ":-/ >> "
@@ -21,10 +21,23 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
 
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Parser errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
