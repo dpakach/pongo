8845532264318d9dedbedfed4e38ba22114e004f
Dipak Acharya <dpakach@gmail.com>
2019-10-29 09:33:27 +0545

    Add function support
    The interpreter now supports functions. The functions can be used as first class variables and also support closures.

--- object/object.go
+++ object/object.go
@@ -2,6 +2,9 @@ package object
 
 import (
 	"fmt"
+	"bytes"
+	"strings"
+	"gorilla/ast"
 )
 
 type ObjectType string
@@ -12,6 +15,7 @@ const (
 	NULL_OBJ = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface {
@@ -53,3 +57,28 @@ type Error struct {
 
 func (e *Error) Type() ObjectType {return ERROR_OBJ}
 func (e *Error) Inspect() string {return "ERROR: "  + e.Message}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Type() ObjectType {return FUNCTION_OBJ}
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("{")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n")
+
+	return out.String()
+}
